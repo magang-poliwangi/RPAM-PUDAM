d2075911852e5f7afef0f65c9030b71c037dc406
NofaFirdaus <nofafirdaus17@gmail.com>
2026-07-23 14:22:27 +0700

    fix url api

--- fe/src/api/axios-instance.js
+++ fe/src/api/axios-instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { getToken, putAccessToken } from "../utils/local-storage";
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api";
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
 export const axiosInstance = axios.create({
     baseURL: BASE_URL,
     withCredentials: true
